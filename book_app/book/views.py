7ce9bf6b765249111acabad51a80967904944a07
Omoleye Julius <joshkid610@gmail.com>
2020-12-02 23:25:52 +0100

    working form post

--- book_app/book/views.py
+++ book_app/book/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Book
 # Create your views here.
 def index(request):
@@ -6,4 +6,17 @@ def index(request):
 	context = {
 	'books':books
 	}
+	if request.method=='POST':
+			author = request.POST.get('author')
+			name = request.POST.get('name')
+			isbn = request.POST.get('isbn')
+			url = request.POST.get('url')
+			new_book = Book(name=name,author=author,isbn=isbn,book_url=url)
+			new_book.save()
+
+			return redirect('/')
+
+		
+
+		
 	return render(request, 'index.html',context)
